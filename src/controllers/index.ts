467052b818fdb900fcfe6f246a21082112531611
cypherpulse <johnbradill67@gmail.com>
2025-12-02 12:44:31 +0300

    feat(controllers): export all controllers for Web3 blockchain technology API

---
+++ src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './adminController';
+export * from './eventController';
+export * from './creatorController';
+export * from './builderController';
+export * from './resourceController';
+export * from './contactController';
+export * from './newsletterController';

1105b768d0b6f8f05f0fc09ceb6953034b2b3abb
cypherpulse <johnbradill67@gmail.com>
2025-12-02 12:44:25 +0300

    feat(services): export all services for Web3 blockchain technology platform

---
+++ src/services/index.ts
@@ -0,0 +1,10 @@
+export { sendEmail, verifyEmailConnection } from './emailService';
+export {
+  configureCloudinary,
+  uploadImage,
+  uploadImageFromUrl,
+  deleteImage,
+  getOptimizedUrl,
+  verifyCloudinaryConnection,
+  cloudinary,
+} from './cloudinaryService';
